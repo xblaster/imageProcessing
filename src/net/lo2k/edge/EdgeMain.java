eced1792165285e53e9a178b54c05d3fe39b6395
Jerome Wax <nospam@lo2k.net>
2011-03-28 17:23:03 +0200

    add CannyEdgeDetector :) pretty cool algorythm

--- src/net/lo2k/edge/EdgeMain.java
+++ src/net/lo2k/edge/EdgeMain.java
@@ -10,52 +10,54 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class EdgeMain extends JPanel {
-	
-	
 
 	public static void main(String args[]) throws MalformedURLException,
 			IOException {
 
 		BufferedImage imageOne = ImageIO
 				.read(new URL(
-						"http://farm6.static.flickr.com/5293/5503399835_7a7fe33742_b.jpg"));
-
+						"http://farm2.static.flickr.com/1400/5136927929_e0ae2b21c7_b.jpg"));
 
 		BufferedImage imageTwo = new BufferedImage(imageOne.getWidth(),
 				imageOne.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-		for (int x = 0; x < imageOne.getWidth(); x++) {
+		/*
+		 * for (int x = 1; x < imageOne.getWidth()-1; x++) {
+		 * 
+		 * for (int y = 1; y < imageOne.getHeight()-1; y++) {
+		 * 
+		 * int greyScale = ImgUtil.getGreyScale(imageOne, x, y); int
+		 * greyScaleAround = 0; for (int i = -1; i <= 1; i++) { for (int j = -1;
+		 * j <= 1; j++) { if ((i!=0 && (j!=0))) { greyScaleAround+=
+		 * ImgUtil.getGreyScale(imageOne, x+i, y+j); } } } greyScaleAround =
+		 * greyScaleAround/8;
+		 * 
+		 * 
+		 * if (Math.abs(greyScaleAround-greyScale)>25) { imageTwo.setRGB(x, y,
+		 * ImgUtil.toRGB(255, 255, 255)); } else { imageTwo.setRGB(x, y,
+		 * ImgUtil.toRGB(0 , 0, 0)); }
+		 * 
+		 * 
+		 * }
+		 * 
+		 * }
+		 */
 
-			for (int y = 0; y < imageOne.getHeight(); y++) {
+		CannyEdgeDetector detector = new CannyEdgeDetector();
+		detector.setSourceImage(imageOne);
+		/*detector.setLowThreshold(0.5f);
+		detector.setHighThreshold(1f);*/
 
-				/* Get the values from imageOne */
+		detector.process();
+		imageTwo = detector.getEdgesImage();
 
-				int rgb = imageOne.getRGB(x, y);
+		JFrame frame = new JFrame();
 
-				// int green = imageOne.getGreen(x,y);
+		frame.add(new DisplayPanel(imageTwo));
 
-				// int blue = imageOne.getBlue(x,y);
-
-				/* Put these values into imageTwo */
-				 int red = ((rgb >> 16) & 255);
-			     int green = ((rgb >> 8) & 255) ;
-			     int blue = (rgb & 255);
-
-			     
-			    int greyScale = ImgUtil.getGreyScale(imageOne, x, y); 
-				imageTwo.setRGB(x, y, ImgUtil.toRGB(greyScale, greyScale, greyScale));
-
-			}
-
-		}
-		
-		 JFrame frame = new JFrame();
-		 
-		 frame.add(new DisplayPanel(imageTwo));
-		 
-		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 frame.setVisible(true);
-		 frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.pack();
 
 	}
 
