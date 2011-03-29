d0a2a6268b9a76c7dd331f0e4f3cb050f14818ba
Jérôme Wax <nospam@lo2k.net>
2011-03-29 21:42:00 +0200

    fix serveral bad algo ;)

--- src/net/lo2k/edge/HausdorffDistance.java
+++ src/net/lo2k/edge/HausdorffDistance.java
@@ -11,11 +11,17 @@ public class HausdorffDistance {
 		int width = groupA.getWidth();
 		int height = groupA.getHeight();
 		
+		
 		for (int i = 0; i < width; i++) {
+			double offset = height / 3;
 			for (int j = 0; j < height; j++) {
 				if (ImgUtil.isWhitePixel(groupA, i, j)) {
 					nbWhitePoint++;
-					distance += calculateNeareastPointDistance(groupB,i,j);
+					double localDistance = calculateNeareastPointDistance(groupB,i,j,(int)offset); 
+					distance += localDistance;
+					offset = localDistance;
+				} else {
+					offset += 1d;
 				}
 			}
 		}
@@ -26,28 +32,35 @@ public class HausdorffDistance {
 	}
 
 
-	private double calculateNeareastPointDistance(BufferedImage groupB2, int x, int y) {
+	private double calculateNeareastPointDistance(BufferedImage groupB2, int x, int y, int offset) {
 		System.out.println("Nearest for " + x + "," + y);
 		if (ImgUtil.isWhitePixel(groupB2, x, y)) {
 			return 0l;
 		}
 		double bestDistance = Long.MAX_VALUE;
-		for (int i = 0; i < groupB2.getWidth(); i++) {
-			for (int j = 0; j < groupB2.getHeight(); j++) {
+		/*for (int i = 0; i < groupB2.getWidth(); i++) {
+			for (int j = 0; j < groupB2.getHeight(); j++) {*/
+		for (int i = x-offset/2; i < x+offset/2; i++) {
+			for (int j = y-offset/2; j < y+offset/2; j++) {
 				if (ImgUtil.isWhitePixel(groupB2, i, j)) {
 					//small optimisation
 					if (Math.abs(i - x) < bestDistance) {
 						if (Math.abs(j - y) < bestDistance) {
-							double euclidianDistance = Math.sqrt((i + x) ^ 2
-									+ (j + y) ^ 2);
+							double euclidianDistance = Math.sqrt((i - x) ^ 2
+									+ (j - y) ^ 2);
 
 							if (euclidianDistance < bestDistance) {
+								System.out.println("Best at "+x+", "+y+" -> "+euclidianDistance);
 								bestDistance = euclidianDistance;
 							}
 						}
 					}
 				}
 			}
+		}
+		System.out.println(bestDistance);
+		if (bestDistance == Long.MAX_VALUE) {
+			return offset+1d;
 		}
 
 		return bestDistance;
